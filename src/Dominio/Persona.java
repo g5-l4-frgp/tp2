ff10f600291ab4b9425a9101c773d66917666287
g5-l4-frgp <mdquin@hotmail.com>
2020-08-29 18:33:20 -0300

    punto uno y dos

---
+++ src/Dominio/Persona.java
@@ -0,0 +1,33 @@
+package Dominio;
+
+public class Persona {
+	
+	private String Nombre;
+	private String Apellido;
+	private int DNI;
+	
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public int getDNI() {
+		return DNI;
+	}
+	public void setDNI(int dNI) {
+		DNI = dNI;
+	}
+
+	
+	 
+	
+
+}
