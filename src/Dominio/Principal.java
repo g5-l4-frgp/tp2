e0c0f647f6a2b4d83576e002bd1b2f94b343847a
g5-l4-frgp <mdquin@hotmail.com>
2020-09-01 17:52:20 -0300

    actualizacion punto 5

--- src/Dominio/Principal.java
+++ src/Dominio/Principal.java
@@ -1,47 +1,36 @@
 package Dominio;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.TreeSet;
 
 public class Principal {
 
-	public static void main(String[] args) {
+@SuppressWarnings("null")
+public static void main(String[] args) {
 
-		TreeSet<Persona>lista=new TreeSet<Persona>();
-		Archivo archivo= new Archivo();
-		archivo.setRUTA("PersonasEmpresa.txt");
+Collator comparador = Collator.getInstance();
+comparador.setStrength(Collator.PRIMARY);
+Archivo archivo= new Archivo();
+TreeSet<Persona>lista=new TreeSet<Persona>();
+archivo.setRUTA("PersonasEmpresa.txt");
+ArrayList<Persona> listarray= new ArrayList();
+System.out.println(" ARCHIVO PERSONASEMPRESA : ");
 
-		System.out.println(" ARCHIVO PERSONASEMPRESA : ");
-		
-			lista=archivo.Leer();
+//Guardo la lista treeSet sin duplicados
+lista=archivo.Leer();
 
-			for(Persona e:lista)
-			{
-				System.out.println(e.getNombre());
-			}
-			
-		
-
-		/*Persona archivo2= new Persona();
-		archivo2.setRuta("Resultado.txt");
-
-		if(!archivo2.existe())
-		{
-		archivo2.creararchivonuevo();
-		}
-
-		//comprobar que funcione el Validador de dni
-
-		Persona a1= new Persona();
-		a1.setDni(12345678);
-		try
-		{
-		   Persona.validarDNI(a1.getDni());
-		   
-		}
-		catch(ExcepcionDNI e)
-		{
-		e.printStackTrace();
-		} */
-	}
-
+//Convierto la lista treeset a ArrayList
+for(Persona e:lista)
+{
+listarray.add(e);
+}
+//Ordeno alfabeticamente
+Collections.sort(listarray,(x, y) -> comparador.compare(x.getNombre(), y.getNombre()));
+for(Persona e:listarray)
+{
+System.out.println(e.getNombre()+" "+e.getDNI());
+}
+}
 }
